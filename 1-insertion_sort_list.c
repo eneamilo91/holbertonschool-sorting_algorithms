7ae07b888099ba0a3e0bab5e58883c6adf8e35bb
eneamilo514 <eneamilo514@gmail.com>
2024-01-11 08:33:17 -0800

    added the 3-rd file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,21 +2,21 @@
 
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-    listint_t *prev_node1 = node1->prev;
-    listint_t *next_node2 = node2->next;
+	listint_t *prev_node1 = node1->prev;
+	listint_t *next_node2 = node2->next;
 
-    if (prev_node1 != NULL)
-        prev_node1->next = node2;
-    else
-        *list = node2;
+	if (prev_node1 != NULL)
+		prev_node1->next = node2;
+	else
+		*list = node2;
 
-    if (next_node2 != NULL)
-        next_node2->prev = node1;
-    
-    node2->prev = prev_node1;
-    node1->next = next_node2;
-    node2->next = node1;
-    node1->prev = node2;
+	if (next_node2 != NULL)
+		next_node2->prev = node1;
+
+	node2->prev = prev_node1;
+	node1->next = next_node2;
+	node2->next = node1;
+	node1->prev = node2;
 }
 
 
--- sort.h
+++ sort.h
@@ -24,6 +24,10 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *first, listint_t *second);
+void quick_sort(int *array, size_t size);
+int arrangement(int *array, int min_index, int pivot_index, size_t size);
+void lomuto_sorting(int *array, int min_index, int pivot_index, size_t size);
 
 #endif
