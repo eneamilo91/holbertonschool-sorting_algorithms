eee859a479f12c635ed77ddfc9a50a611d387c41
MDrenova <mateodrenova@gmail.com>
2024-01-11 04:03:45 -0800

    Fix betty style

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,18 +8,19 @@
 
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-    if (node1->prev)
-        node1->prev->next = node2;
-    if (node2->next)
-        node2->next->prev = node1;
-    node1->next = node2->next;
-    node2->prev = node1->prev;
+	if (node1->prev)
+		node1->prev->next = node2;
+	if (node2->next)
+		node2->next->prev = node1;
 
-    node2->next = node1;
-    node1->prev = node2;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
 
-    if (node2->prev == NULL)
-        *list = node2;
+	node2->next = node1;
+	node1->prev = node2;
+
+	if (node2->prev == NULL)
+		*list = node2;
 }
 
 /**
